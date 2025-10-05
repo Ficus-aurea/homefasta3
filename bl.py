70fba11d64e5cc0f3df0b3f841fd8e488621a55b
Ficus-aurea <bannaaanna7@gmail.com>
2025-10-06 02:40:34 +0300

    Update bl.py

--- bl.py
+++ bl.py
@@ -1,23 +1,9 @@
-from fasta import FastaReader
+from fasta import Seq, FastaReader
 
-def main():
-    path = r"/home/anna/Документы/Anna/lab3sem(oop)/examples/Pezoporus_wallicus_exampels.fasta"
-    try:
-        fasta= FastaReader(path)
+seq = Seq("test", "ATCGTUAACG")
+print(f"Длина: {len(seq)}")
+print(f"Тип: {seq.alphabet()}")
 
-        if not fasta.is_fasta():
-            return 'Указанный файл не является файлом формата Fasta'
-
-        for i, seq_record in enumerate(fasta.read_records(), 1):
-            print(f'Запись #{i}:')
-            print(seq_record)
-            print('-' * 50)
-
-            if i >= 3:
-                print('И так далее...')
-                break
-    except Exception as e:
-        print(f'Произошла ошибка {e}')
-
-if __name__ == "__main__":
-    main()
+reader_one = FastaReader("Pezoporus_wallicus_exampels.fasta")
+for sequence in reader_one:
+    print(sequence)
