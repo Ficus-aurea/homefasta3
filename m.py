3afd477b911651906c1a9992d682fc9bcafbaa2a
Ficus-aurea <bannaaanna7@gmail.com>
2025-10-06 03:06:22 +0300

    Add files via upload

---
+++ m.py
@@ -0,0 +1,31 @@
+from fasta import FastaReader
+
+def main():
+    """
+    Основной модуль демонстрационной программы
+    """
+
+    path = r"/home/anna/Документы/Anna/lab3sem(oop)/examples/Pezoporus_wallicus_exampels.fasta"
+
+    try:
+        fasta = FastaReader(path)
+
+        if not fastф.is_fasta():
+            return 'Указанный файл не является файлом формата Fasta'
+
+        for i, seq_record in enumerate(fasta.read_records(), 1):
+            print(f'Запись #{i}:')
+            print(seq_record)
+            print('-' * 50)
+
+            if i >= 3:
+                print('И так далее...')
+                break
+
+
+    except Exception as e:
+        print(f'Произошла ошибка {e}')
+
+
+if __name__ == "__main__":
+    main()
