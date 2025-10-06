85c98de0a633c445ef3310de9e73ed0b255e3d68
Ficus-aurea <bannaaanna7@gmail.com>
2025-10-06 16:31:07 +0300

    Update bl.py

--- bl.py
+++ bl.py
@@ -1,8 +1,8 @@
-from Fasta import Seq, FastaReader
+from all import Seq, FastaReader
 
 seq = Seq("test", "ATCGTUAACG")
 print(f"Длина: {len(seq)}")
-print(f"Тип: {seq.alphabet()}")
+print(f"Тип: {seq.alphabet_validation()}")
 
 reader_one = FastaReader("homefasta3/exampels/Pezoporus_wallicus_exampels.fasta")
 for sequence in reader_one:
